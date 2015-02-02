8f12cd330d0dec4fd3837084ea06c3a8d35580e0
Matteo Zino <mzino@hint.club>
2015-02-03 00:21:21 +0100

    Critica

---
+++ m/critica.php
@@ -0,0 +1,10 @@
+<?php
+  $page_title = "Si Alza il Vento - La critica";
+?>
+<?php include("../include/head.php"); ?>
+	<body id="menu-critica">
+		<div class="flex">
+			<?php include("../include/menu.html"); ?>
+			<div class="grid clearfix">
+				<?php include("../include/articles/critica.html"); ?>
+				<?php include("../include/footer.html"); ?>
---
+++ www/critica_2.php
@@ -0,0 +1,10 @@
+<?php
+  $page_title = "Si Alza il Vento - La critica";
+?>
+<?php include("../include/head.php"); ?>
+	<body id="menu-critica2">
+		<div class="flex">
+			<?php include("../include/menu.html"); ?>
+			<div class="grid clearfix">
+				<?php include("../include/articles/critica_2.html"); ?>
+				<?php include("../include/footer.html"); ?>
