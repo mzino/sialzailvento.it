5ee5798c42bd72057833494dfd330aa0464dec82
Matteo Zino <mzino@hint.club>
2015-02-26 20:47:15 +0100

    Added Facebook thumbnail and description

--- include/head.php
+++ include/head.php
@@ -5,6 +5,9 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="Si Alza il Vento">
+		<meta property="og:image" content="../assets/fb_thumb.jpg" />
+		<meta property="og:title" content="Si Alza il Vento: Un film per il cuore ancora prima che per gli occhi" />
+		<meta property="og:description" content="Analisi personale dell'opera e delle sue tematiche, a cura di Andrea Di Nuzzo e Hintclub S.r.l.s." />
 		<link rel="stylesheet" href="css/normalize.css">
 		<link rel="stylesheet" href="css/style.css">
 		<link href='http://fonts.googleapis.com/css?family=Lato:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
